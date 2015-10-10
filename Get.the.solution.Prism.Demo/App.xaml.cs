038be872287e7169e5ec91781be7545e6c105fc6
mfe- <mfe@live.de>
2015-10-10 12:52:45 +0200

    Catch UnhandledException

--- Get.the.solution.Prism.Demo/App.xaml.cs
+++ Get.the.solution.Prism.Demo/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,10 +19,49 @@ namespace Get.the.solution.Prism.Demo
         {
             base.OnStartup(e);
 
+#if (DEBUG)
             Bootstrapper bootstrapper = new Bootstrapper();
             bootstrapper.Run();
+#else
+            RunInReleaseMode();
+#endif
 
             this.ShutdownMode = ShutdownMode.OnMainWindowClose;
+        }
+        private static void RunInReleaseMode()
+        {
+            AppDomain.CurrentDomain.UnhandledException += AppDomainUnhandledException;
+            try
+            {
+                Bootstrapper bootstrapper = new Bootstrapper();
+                bootstrapper.Run();
+            }
+            catch (Exception ex)
+            {
+                HandleException(ex);
+            }
+        }
+
+        private static void AppDomainUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            HandleException(e.ExceptionObject as Exception);
+        }
+
+        private static void HandleException(Exception ex)
+        {
+            if (ex == null)
+                return;
+
+            string filePath = "Error.txt";
+
+            using (StreamWriter writer = new StreamWriter(filePath, true))
+            {
+                writer.WriteLine("Message :" + ex.Message + "<br/>" + Environment.NewLine + "StackTrace :" + ex.StackTrace +
+                   "" + Environment.NewLine + "Date :" + DateTime.Now.ToString());
+                writer.WriteLine(Environment.NewLine + "-----------------------------------------------------------------------------" + Environment.NewLine);
+            }
+
+            Environment.Exit(1);
         }
     }
 }
